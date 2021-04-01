666ee6d9f4e78726f6955d7b5cd730d6d42746ff
Joseph Grainger <grrainger7@gmail.com>
2021-04-01 10:04:54 +0200

    finished

---
+++ file.js
@@ -0,0 +1,14 @@
+// 1.
+
+const myStringVariable = "Good morrrrrrningggg";
+const myNumberVariable = 35;
+
+console.log(myStringVariable + " everyone my name is joe and in 5 years i will be " + myNumberVariable + " years old");
+
+// The + symbol is connecting the strings and number variables together
+
+// 2. 
+
+const myNewString = "1005";
+const myNewStringAsANumber = parseInt(myNewString);
+console.log(myNewStringAsANumber);
